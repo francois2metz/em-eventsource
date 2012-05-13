888ddc8c0d0ce3c689474f9187b7fb3e814dc63b
François de Metz <francois@2metz.fr>
2012-05-13 11:52:43 +0200

    Add flowdock example.
    Signed-off-by: François de Metz <francois@stormz.me>

---
+++ examples/flowdock.rb
@@ -0,0 +1,22 @@
+# https://flowdock.com/
+
+
+token = "" # token = your Flowdock access token
+organization = "" # organization = Flowdock subdomain
+flow = "" # flow = name of flow in URL
+
+$: << 'lib' << '../lib'
+
+require 'em-eventsource'
+
+EM.run do
+  source = EM::EventSource.new("https://#{token}:@stream.flowdock.com/flows/#{organization}/#{flow}",
+                               {query: 'text/event-stream'},
+                               {'Accept' => 'text/event-stream'})
+
+  source.message do |message|
+    puts "new message #{message}"
+  end
+
+  source.start
+end
